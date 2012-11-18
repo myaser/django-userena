e7beadd05bb54ff20da762e58521b72aa54a6f2e
myaser <me.MahmoudYaser@gmail.com>
2012-11-18 13:47:37 +0200

    removing debugtoolbar, django extentions from settings

--- demo_project/settings.py
+++ demo_project/settings.py
@@ -72,7 +72,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'userena.middleware.UserenaLocaleMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+#    'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 TEMPLATE_CONTEXT_PROCESSORS = (
     "django.contrib.auth.context_processors.auth",
